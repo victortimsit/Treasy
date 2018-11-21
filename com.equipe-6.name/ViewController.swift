bf5096a3f4f221201a63a98145411a5d84315f87
vtimsit <victor.timsit@hetic.net>
2018-11-21 07:29:38 +0100

    Initial Commit

---
+++ com.equipe-6.name/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  com.equipe-6.name
+//
+//  Created by Victor Timsit on 21/11/2018.
+//  Copyright © 2018 Victor Timsit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
