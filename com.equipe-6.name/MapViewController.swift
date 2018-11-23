1686867dcf5436195b6aca2751d8fdbd8af74d58
vtimsit <victor.timsit@hetic.net>
2018-11-23 20:15:29 +0100

    :init: Initial commit

---
+++ com.equipe-6.name/MapViewController.swift
@@ -0,0 +1,103 @@
+//
+//  MapViewController.swift
+//  com.equipe-6.name
+//
+//  Created by Victor Timsit on 21/11/2018.
+//  Copyright © 2018 Victor Timsit. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate, UITextFieldDelegate {
+
+    @IBOutlet weak var destinationTextField: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
+    
+//    @IBAction func closeKeyboard(sender: MKMapView) {
+//        if whereTextField.isFirstResponder == true {
+//            whereTextField.resignFirstResponder()
+//        }
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        destinationTextField.delegate = self
+        
+        
+        // Do any additional setup after loading the view.
+        mapView.showsUserLocation = true
+        
+        if CLLocationManager.locationServicesEnabled() == true {
+            
+            if CLLocationManager.authorizationStatus() == .restricted ||
+                CLLocationManager.authorizationStatus() == .denied ||
+                CLLocationManager.authorizationStatus() == .notDetermined {
+                
+                locationManager.requestWhenInUseAuthorization()
+                
+            }
+            
+            locationManager.desiredAccuracy = 1.0
+            locationManager.delegate = self
+            locationManager.startUpdatingLocation()
+            
+        } else {
+            print("Please turn on location services or GPS")
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Show the navigation bar on other view controllers
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    //MARK:- CLLocationManager Delegates
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+        self.mapView.setRegion(region, animated: true)
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Unable to access your current location")
+    }
+    
+    //MARK:- UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+//        performSegue(withIdentifier: "search", sender: nil)
+        
+        let secondViewController = self.storyboard.instantiateViewControllerWithIdentifier("result") as result
+        self.navigationController.pushViewController(secondViewController, animated: true)
+        
+        return true
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
